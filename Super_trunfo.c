0b05bbbfa5cd2e04f10a9c3e1c58df96146aa788
Thais-dev-code <tmara.dev@gmail.com>
2025-08-13 10:27:25 -0300

    Update Super_trunfo.c atualização para densidade e pibpercapita

--- Super_trunfo.c
+++ Super_trunfo.c
@@ -9,6 +9,7 @@ int main() {
     int populacao1;
     float area1, pib1;
     int numerodepontosturisticos1;
+    float densidadepopulacional1, pibpercapita1;
 
     //--------Entrada dos dados da carta 01 
 
@@ -20,20 +21,26 @@ int main() {
     printf("Código da Carta (ex: A01):\n ");
     scanf("%s", codigodacarta1);
 
-    printf("Nome da Cidade: ");
-    scanf(" %s", nomedacidade1);
+    printf("Nome da Cidade:\n ");
+    scanf(" %49[^\n]", nomedacidade1);
 
-    printf ("populacao:");
+    printf ("populacao:\n");
     scanf ("%d", &populacao1);
 
-    printf ("area1:");
+    printf ("area1 (km²)\n:");
     scanf ("%f", &area1);
 
-    printf ("pib1:");
-    scanf ("%f", &pib1);
+    float pib1_bilhoes;
+    printf ("pib1 (em Bilhoes de reais):\n");
+    scanf ("%f", &pib1_bilhoes);
+    pib1 = pib1_bilhoes *1e9;
 
-    printf("Numero de pontos turisticos:");
+    printf("Numero de pontos turisticos 1 :\n");
     scanf("%i", &numerodepontosturisticos1);
+     
+   //------- densidade e pib per capita 01 -----
+   densidadepopulacional1= (float)populacao1/area1;
+   pibpercapita1=pib1/(float)populacao1;
 
     //-------- Carta 02 --------
 
@@ -43,7 +50,7 @@ int main() {
     int populacao2;
     float area2, pib2;
     int numerodepontosturisticos2;
-
+    float densidadepopulacional2 ,pibpercapita2;
     //--------Entrada dos dados da carta 02
 
     printf("Digite os Dados da Carta 02:\n");
@@ -52,22 +59,30 @@ int main() {
     scanf(" %c", &estado2);
 
     printf("Código da Carta (ex: A02):\n ");
-    scanf("%s", codigodacarta1);
+    scanf("%s", codigodacarta2);
 
-    printf("Nome da Cidade: ");
-    scanf(" %s", nomedacidade2);
+    printf("Nome da Cidade 02:\n ");
+    scanf(" %49[^\n]", nomedacidade2);
 
-    printf ("populacao:");
+    printf ("populacao cidade 02:\n");
     scanf ("%d", &populacao2);
 
-    printf ("area2:");
+    printf ("area 2 (km²):\n");
     scanf ("%f", &area2);
 
-    printf ("pib2:");
-    scanf ("%f", &pib2);
+    float pib2_bilhoes;
+    printf ("pib 2 (em Bilhoes de reais):\n");
+    scanf ("%f", &pib2_bilhoes);
+    pib2= pib2_bilhoes *1e9;
 
-    printf("Numero de pontos turisticos 2:");
+    printf("Numero de pontos turisticos 2:\n");
     scanf("%i", &numerodepontosturisticos2);
+
+    //----calculo carta 02 ----
+
+    densidadepopulacional2=(float)populacao2/area2;
+    pibpercapita2=pib2/(float)populacao2;
+
 
     // -------- Saida de Dados carta 01
     printf ("\n-----CARTA 01------");
@@ -75,10 +90,11 @@ int main() {
     printf ("\n Código da carta : %s\n", codigodacarta1);
     printf("\n Nome Da Cidade: %s\n", nomedacidade1);
     printf("\n População:%d\n",populacao1);
-    printf("\n Area em m²:%f\n", area1);
-    printf("\nPib cidade 1:%f\n", pib1);
+    printf("\n Area 1 em m²:%.2f/km²\n", area1);
+    printf("\nPib cidade 1:%f Bilhões de reais\n", pib1);
     printf("\n Numero de pontos turisticos na cidade 1 :%i\n", numerodepontosturisticos1);
-
+    printf("Densidade Populacional 1: %.2f hab/km²\n", densidadepopulacional1);
+    printf("PIB per Capita 1: %.2f reais\n", pibpercapita1);
     
     // -------- Saida de Dados carta 02
     printf ("\n-----CARTA 02------");
@@ -86,12 +102,12 @@ int main() {
     printf ("\n Código da carta : %s\n", codigodacarta2);
     printf("\n Nome Da Cidade: %s\n", nomedacidade2);
     printf("\n População:%d\n",populacao2);
-    printf("\n Area em m²:%f\n", area2);
-    printf("\n Pib :%f\n", pib2);
-    printf("\n Numero de pontos turisticos na cidade:%i\n", numerodepontosturisticos2);
-
+    printf("\n Area 2 em m²:%.2f/km²\n", area2);
+    printf("\n Pib :%f Bilhões de reais\n", pib2);
+    printf("\n Numero de pontos turisticos na cidade 2:%i\n", numerodepontosturisticos2);
+    printf("\n Densidade populacional2 : %.2f hab/km²\n", densidadepopulacional2);
+    printf("\n PIB per Capita 2: %.2f reais\n", pibpercapita2);
 
 
     return 0;
 }
-
